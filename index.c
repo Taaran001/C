9a6adf5796c0541d1ee77162ccc366fbd43cabdb
Taaran <taaran06@gmail.com>
2024-11-18 23:11:44 +0530

    1 g

--- index.c
+++ index.c
@@ -104,6 +104,44 @@
 //         return 0;
 //     }
 
+//                  Program 1(g)
+// #include <stdio.h>
+// int main() {
+//     char operator;
+//     double num1, num2, result;
+//     printf("Enter first number: ");
+//     scanf("%lf", &num1);
+//     printf("Enter an operator (+, -, *, /): ");
+//     scanf(" %c", &operator);
+//     printf("Enter second number: ");
+//     scanf("%lf", &num2);
+//     switch(operator) {
+//         case '+':
+//             result = num1 + num2;
+//             printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+//             break;
+//         case '-':
+//             result = num1 - num2;
+//             printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+//             break;
+//         case '/':
+//             if(num2 != 0) {
+//                 result = num1 / num2;
+//                 printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+//             } else {
+//                 printf("Error! Division by zero is not allowed.\n");
+//             }
+//             break;
+//         default:
+//             printf("Error! Invalid operator.\n");
+//     }
+//     return 0;
+// }
+
 
 // 2)
 //                 program 2(a)
@@ -764,5 +802,3 @@
 //     }
 //     return 0;
 // }
-
-
